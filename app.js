df4cd9fff8b292693686840946a3bbcee1ed3bad
AyushiPohekar <ayushi2021.29@gmail.com>
2022-10-30 21:54:15 +0530

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cookiParser=require("cookie-parser")
+require("./db/conn");
+const router = require("./routes/router");
+const cors = require("cors");
+
+
+const app=express();
+const port=process.env.port||8009;
+require("./db/conn");
+
+app.use(express.json());
+app.use(cookiParser());
+app.use(cors());
+
+app.use(router);
+
+app.listen(port,()=>{
+    console.log(`server started at port no:${port}`)
+})
